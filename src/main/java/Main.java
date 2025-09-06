08c8c8a3f037b3896e649d3d8936c47f45512fe5
vedesh95 <vedesh95@gmail.com>
2025-09-06 19:11:19 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -44,8 +44,7 @@ public class Main {
              writeShort(out, (short)35);
              out.flush();
          } else {
-             writeInt(out, (int)19);
-//             out.write(ByteBuffer.allocate(4).putInt(19).array());
+             out.write(ByteBuffer.allocate(4).putInt(19).array());
              writeInt(out, corelation_id);
              writeShort(out, (short) 0); // error code
              writeByte(out, (byte) 2); // array length
